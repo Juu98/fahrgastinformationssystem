78f052173337ccb35b64ef3583721362488c817d
Zdravko Yanakiev <zdravko.yanakiev@mailbox.tu-dresden.de>
2015-10-29 11:06:18 +0100

    Fixed application startup from console

--- prototypes/prototype_timetable/src/main/java/com/prototypes/Prototype1Application.java
+++ prototypes/prototype_timetable/src/main/java/com/prototypes/Prototype1Application.java
@@ -2,8 +2,10 @@ package com.prototypes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:Beans.xml")
 public class Prototype1Application {
 
     public static void main(String[] args) {
