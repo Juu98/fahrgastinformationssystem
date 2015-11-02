f76281205a3377520839ef9294ea4f028aec00c7
Oliver Schmidt <oliver.schmidt3@mailbox.tu-dresden.de>
2015-11-02 12:39:26 +0100

    add possibility to delete ImagePosts

--- prototypes/imageboard/src/main/java/imageboard/ImageboardController.java
+++ prototypes/imageboard/src/main/java/imageboard/ImageboardController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,6 +55,12 @@ class ImageboardController {
             System.out.println("empty");
             return showImages(model);
         }
+        return "redirect:/imageboard";
+    }
+
+    @RequestMapping(value = "/imageboard/{id}", method = RequestMethod.DELETE)
+    public String deleteImagePost( @PathVariable Long id) {
+        imageboard.delete(id);
         return "redirect:/imageboard";
     }
 }
