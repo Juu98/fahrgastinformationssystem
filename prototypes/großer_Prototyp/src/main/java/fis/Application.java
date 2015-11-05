e9a5be8ab767f82290244839d8b383b065038bb5
Oliver Schmidt <oliver.schmidt3@mailbox.tu-dresden.de>
2015-11-05 16:28:34 +0100

    created project for first big prototype

---
+++ prototypes/großer_Prototyp/src/main/java/fis/Application.java
@@ -0,0 +1,14 @@
+package fis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by spiollinux on 05.11.15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
