9448da308a717c7087d93deb0b6543e240177271
Oliver Schmidt <oliver.schmidt3@mailbox.tu-dresden.de>
2015-11-08 13:47:11 +0100

    Run telegram parser as a background thread
    configuration using application.yml now working
    put configuration into it's own class

--- prototypes/socket test/src/Server/ServerTest.java
+++ prototypes/socket test/src/Server/ServerTest.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Created by spiollinux on 06.11.15.
@@ -27,7 +28,11 @@ public class ServerTest {
                 client = server.accept();
                 System.out.println("Incoming connection: accepted");
                 handleConnection(client);
-            } catch (IOException e) {
+            }
+            catch (SocketException e) {
+                System.err.println("Remote Socket closed");
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
             finally {
@@ -40,19 +45,26 @@ public class ServerTest {
     private static void handleConnection(Socket client) throws IOException {
         InputStream in = client.getInputStream();
         OutputStream out = client.getOutputStream();
-        byte[] message = new byte[255];
-        message[0] = (byte) 0xFF;
-        message[1] = (byte) 0xFF;
-        message[2] = (byte) 0xFF;
-        message[3] = (byte) 2; //Länge
-        message[4] = (byte) 1; //Typ
-        message[5] = (byte) 42;
+        while (true) {
+            byte[] message = new byte[255];
+            message[0] = (byte) 0xFF;
+            message[1] = (byte) 0xFF;
+            message[2] = (byte) 0xFF;
+            message[3] = (byte) 2; //Länge
+            message[4] = (byte) 1; //Typ
+            message[5] = (byte) 42;
 
-        for (int i = 6; i < 255; ++i) {
-            message[i] = (byte) 0x00;
+            for (int i = 6; i < 255; ++i) {
+                message[i] = (byte) 0x00;
+            }
+
+            out.write(message);
+            out.flush();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
-        out.write(message);
-        out.flush();
     }
 }
