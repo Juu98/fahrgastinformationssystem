cc7565b33cb132dcf89c208b0b3aedd61ac78636
Zdravko Yanakiev <zdravko.yanakiev@mailbox.tu-dresden.de>
2015-11-24 18:41:37 +0100

    Changes in RailML parser added Javadoc comments removed unneeded code

--- app/src/main/java/fis/railmlparser/RailMLParser.java
+++ app/src/main/java/fis/railmlparser/RailMLParser.java
@@ -8,23 +8,39 @@ import org.railml.schemas._2009.Railml;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/*
- * Maps RailML data to Java objects.
+/**
+ * Main RailML parser class. Maps RailML data to Java objects.
+ * 
+ * @author Zdravko
  */
 public class RailMLParser {
     private ApplicationContext appContext;
     private XMLConverter converter;
 
-    // Initialize beans and ignore JAXB2 warnings.
+    /**
+     * Sole constructor, initializes beans and sets a custom
+     * ValidationEventHandler in order to ignore JAXB2 warnings.
+     */
     public RailMLParser() {
 	appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 	converter = (XMLConverter) appContext.getBean("XMLConverter");
 	converter.getUnmarshaller().setValidationEventHandler(new CustomValidationEventHandler());
     }
 
-    /*
-     * Converts a RailML file and returns a Railml root object which can be used
-     * to access all data in the file.
+    /**
+     * Converts a RailML file into an object tree and returns a Railml root
+     * object which can be used to access all data in the generated tree
+     * structure.
+     * 
+     * @param railMLpath
+     *            Path of RailML file in the classpath.
+     * 
+     * @return Railml root object.
+     * 
+     * @throws IOException
+     *             If an input or output exception occurred.
+     * @throws JAXBException
+     *             If an internal JAXB Exception occurred.
      */
     public Railml parseRailML(String railMLPath) throws IOException, JAXBException {
 	return (Railml) converter.convertFromXMLToObject(railMLPath);
--- app/src/main/java/fis/railmlparser/XMLConverter.java
+++ app/src/main/java/fis/railmlparser/XMLConverter.java
@@ -1,53 +1,51 @@
 package fis.railmlparser;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.xml.bind.JAXBException;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import org.springframework.core.io.ClassPathResource;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-/* 
+/**
  * Class used by RailMLParser to unmarshal XML data. Exists as a bean.
+ * 
+ * @author Zdravko
  */
 public class XMLConverter {
 
-    private Jaxb2Marshaller marshaller;
     private Jaxb2Marshaller unmarshaller;
 
-    public Jaxb2Marshaller getMarshaller() {
-	return marshaller;
-    }
-
-    public void setMarshaller(Jaxb2Marshaller marshaller) {
-	this.marshaller = marshaller;
-    }
-
+    /**
+     * 
+     * @return Current unmarshaller.
+     */
     public Jaxb2Marshaller getUnmarshaller() {
 	return unmarshaller;
     }
 
+    /**
+     * 
+     * @param unmarshaller
+     *            Unmarshaller to set.
+     */
     public void setUnmarshaller(Jaxb2Marshaller unmarshaller) {
 	this.unmarshaller = unmarshaller;
     }
 
-    public void convertFromObjectToXML(Object object, String filepath) throws IOException, JAXBException {
-	FileOutputStream os = null;
-	try {
-	    os = new FileOutputStream(filepath);
-	    getMarshaller().marshal(object, new StreamResult(os));
-	} finally {
-	    if (os != null) {
-		os.close();
-	    }
-	}
-    }
-
+    /**
+     * 
+     * @param xmlfile
+     *            XML file to be unmarshalled.
+     * @return Root object of tree data structure.
+     * @throws IOException
+     *             If an input or output exception occurred.
+     * 
+     * @throws JAXBException
+     *             If an internal JAXB Exception occurred.
+     */
     public Object convertFromXMLToObject(String xmlfile) throws IOException, JAXBException {
 	InputStream is = null;
 	try {
