e4cc9029ef931094f673f93fb9c69aea9b62d832
Oliver Schmidt <oliver.schmidt3@mailbox.tu-dresden.de>
2015-12-06 14:49:35 +0100

    ClientStatusTelegram implements SendableTelegram

--- app/src/main/java/fis/telegrams/ClientStatusTelegram.java
+++ app/src/main/java/fis/telegrams/ClientStatusTelegram.java
@@ -3,5 +3,9 @@ package fis.telegrams;
 /**
  * Created by spiollinux on 06.12.15.
  */
-public class ClientStatusTelegram {
+public class ClientStatusTelegram implements SendableTelegram{
+	@Override
+	public byte[] getRawTelegram() {
+		return new byte[255];
+	}
 }
