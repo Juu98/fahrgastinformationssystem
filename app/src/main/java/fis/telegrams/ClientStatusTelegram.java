f635d5c399ff81c21e99e89513da46735c28dacf
Oliver Schmidt <oliver.schmidt3@mailbox.tu-dresden.de>
2015-12-06 21:52:26 +0100

    add first unittest for ClientStatusTelegram (currently failing)

--- app/src/main/java/fis/telegrams/ClientStatusTelegram.java
+++ app/src/main/java/fis/telegrams/ClientStatusTelegram.java
@@ -4,6 +4,14 @@ package fis.telegrams;
  * Created by spiollinux on 06.12.15.
  */
 public class ClientStatusTelegram implements SendableTelegram{
+	private String ID;
+	private byte status;
+
+	public ClientStatusTelegram(String ID, byte status) {
+		this.ID = ID;
+		this.status = status;
+	}
+
 	@Override
 	public byte[] getRawTelegram() {
 		return new byte[255];
---
+++ app/src/test/java/fis/telegrams/ClientStatusTelegramTest.java
@@ -0,0 +1,39 @@
+package fis.telegrams;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by spiollinux on 06.12.15.
+ */
+public class ClientStatusTelegramTest {
+
+	@Test
+	public void testGetRawTelegram() throws Exception {
+		String ID ="FIS";
+		byte status = (byte) 1;
+		ClientStatusTelegram tele = new ClientStatusTelegram(ID, status);
+		//construct a rawTelegram
+		byte[] rawReferenceTelegram = new byte[Telegram.rawTelegramLength];
+		byte[] rawID = ID.getBytes("windows-1252");
+		int i = 0;
+		for(; i < 3; ++i) {
+			rawReferenceTelegram[i] = (byte) 255;
+		}
+		//skipping length byte for now
+		i++;
+		rawReferenceTelegram[i++] = (byte) 243;
+		for(; i < 12; ++i) {
+			int j = i - 5;
+			if(j < rawID.length)
+				rawReferenceTelegram[i] = rawID[j];
+			else
+				rawReferenceTelegram[i] = 0;
+		}
+		rawReferenceTelegram[i] = status;
+
+		//compare
+		assertArrayEquals("ClientStatusTelegram doesn't have expected value", rawReferenceTelegram, tele.getRawTelegram());
+	}
+}
