8e1077028a7692f20019d97a3b7e583cab5d985b
kloppstock <jonas@groeger-clan.de>
2015-12-07 15:45:27 +0100

    added telegram classes with tests (and javadoc)

--- app/src/main/java/fis/telegrams/LabTimeTelegram.java
+++ app/src/main/java/fis/telegrams/LabTimeTelegram.java
@@ -1,7 +1,30 @@
 package fis.telegrams;
 
+import java.time.LocalTime;
+
 /**
- * Created by spiollinux on 12.11.15.
+ * @author spiollinux, kloppstock
+ * Eine Klasse für Laborzeittelegramme.
  */
 public class LabTimeTelegram extends Telegram {
+	private LocalTime time;
+	
+	/**
+	 * Konstruktor für das Laborzeittelegramm. 
+	 * @param time (Zeit)
+	 * @throws NullPointerException
+	 */
+	public LabTimeTelegram(LocalTime time) throws NullPointerException {
+		if(time == null)
+			throw new NullPointerException();
+		this.time = time;
+	}
+	
+	/**
+	 * Getter für time.
+	 * @return	time (Zeit)
+	 */
+	public LocalTime getTime(){
+		return this.time;
+	}
 }
---
+++ app/src/test/java/fis/telegrams/LabTimeTelegramTest.java
@@ -0,0 +1,33 @@
+package fis.telegrams;
+
+import java.time.LocalTime;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LabTimeTelegramTest {
+	private LabTimeTelegram telegram;
+	private LocalTime time;
+	
+	@Before
+	public void setup(){
+		this.time = LocalTime.of(13, 37);
+	}
+	
+	@Test
+	public void NullConstructorTest(){
+		boolean exceptionCatched = false;
+		try{
+			this.telegram = new LabTimeTelegram(null);
+		} catch(NullPointerException e) {
+			exceptionCatched = true;
+		}
+		assertTrue("The constructor should throw a NullPointerException if given a null parameter!", exceptionCatched);
+	}
+	
+	@Test
+	public void ConstructorAndGetterTest(){
+		this.telegram = new LabTimeTelegram(this.time);
+		assertEquals(this.time, this.telegram.getTime());
+	}
+}
