f041cba093716ca8ff8f7581682c5b0dd0e726bb
Oliver Schmidt <oliver.schmidt3@mailbox.tu-dresden.de>
2016-01-03 00:41:06 +0100

    RailML parsing only initiated if receiver was ONLINE before
    contributes to #66

---
+++ app/src/main/java/fis/common/EventTranslator.java
@@ -0,0 +1,48 @@
+package fis.common;
+
+import fis.data.TimetableEvent;
+import fis.data.TimetableEventType;
+import fis.telegramReceiver.ConnectionStatus;
+import fis.telegramReceiver.ConnectionStatusEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Übersetzt Events von Komponenten in Events, die von anderen Komponenten verarbeitet werden können
+ */
+@Component
+public class EventTranslator implements ApplicationEventPublisherAware {
+	private ApplicationEventPublisher publisher;
+	private boolean hasBeenOnlineBefore;
+
+	public EventTranslator() {
+		//initial parsing of RailML is needed if Receiver is offline
+		this.hasBeenOnlineBefore = true;
+	}
+
+	/**
+	 * Übersetzt ConnectionStatusEvents in TimetableEvents
+	 * @param event
+	 */
+	@EventListener
+	public void notifyReceiverOffline(ConnectionStatusEvent event) {
+		ConnectionStatus receivedStatus = event.getStatus();
+		if (receivedStatus.equals(ConnectionStatus.OFFLINE)) {
+			if (this.hasBeenOnlineBefore) {
+				publisher.publishEvent(new TimetableEvent(TimetableEventType.parseRailML));
+			}
+			this.hasBeenOnlineBefore = false;
+		}
+		else if (receivedStatus.equals(ConnectionStatus.ONLINE)) {
+			this.hasBeenOnlineBefore = true;
+			publisher.publishEvent(new TimetableEvent(TimetableEventType.cleanup));
+		}
+	}
+
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.publisher = applicationEventPublisher;
+	}
+}
