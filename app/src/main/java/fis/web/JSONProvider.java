39f3d0b58d0f180f79ae12abc807ab1ba33645a8
kloppstock <jonas@groeger-clan.de>
2016-01-04 22:27:49 +0100

    added javadoc to jsonprovider

--- app/src/main/java/fis/web/JSONProvider.java
+++ app/src/main/java/fis/web/JSONProvider.java
@@ -16,10 +16,20 @@ import java.util.List;
  * @author Robert
  */
 public class JSONProvider {
+	/**
+	 * Eine Klasse für die JSON optimierten Bahnhofsdaten. 
+	 * @author Robert, kloppstock
+	 */
 	public static class StationView {
 		private String id;
 		private String name;
 
+		/**
+		 * Konstruktor für die Bahnhofsdatenstruktur.
+		 * @param id
+		 * @param name
+		 * @throws IllegalArgumentException
+		 */
 		public StationView(String id, String name) throws IllegalArgumentException {
 			if (id == null || name == null)
 				throw new IllegalArgumentException("One of the given parameters is null!"); // TODO:
@@ -30,6 +40,11 @@ public class JSONProvider {
 			this.name = name;
 		}
 
+		/**
+		 * Konstruktor für die Bahnhofsdatenstruktur.
+		 * @param s (Station)
+		 * @throws IllegalArgumentException
+		 */
 		public StationView(Station s) throws IllegalArgumentException {
 			if (s == null)
 				throw new IllegalArgumentException("The given parameter is null!"); // TODO:
@@ -40,10 +55,18 @@ public class JSONProvider {
 			this.name = s.getLongName();
 		}
 
+		/**
+		 * Getter für id
+		 * @return id
+		 */
 		public String getId() {
 			return id;
 		}
 
+		/**
+		 * Getter für name
+		 * @return name
+		 */
 		public String getName() {
 			return name;
 		}
@@ -69,12 +92,25 @@ public class JSONProvider {
 
 	}
 
+	/**
+	 * Eine Klasse für die JSON optimierten Zuglaufsdaten. 
+	 * @author Robert, kloppstock
+	 *
+	 */
 	public static class TrainRouteView {
 		private String id;
 		private String name;
 		private StationView begin;
 		private StationView end;
 
+		/**
+		 * Konstruktor für die Zuglaufdatenstruktur.
+		 * @param id
+		 * @param begin
+		 * @param name
+		 * @param end
+		 * @throws IllegalArgumentException
+		 */
 		public TrainRouteView(String id, StationView begin, String name, StationView end)
 				throws IllegalArgumentException {
 			if (id == null || begin == null || name == null || end == null)
@@ -88,6 +124,11 @@ public class JSONProvider {
 			this.end = end;
 		}
 
+		/**
+		 * Konstruktor für die Bahnhofsdatenstruktur.
+		 * @param tr (Zuglauf)
+		 * @throws IllegalArgumentException
+		 */
 		public TrainRouteView(TrainRoute tr) throws IllegalArgumentException {
 			if (tr == null)
 				throw new IllegalArgumentException("The given parameter is null!"); // TODO:
@@ -100,18 +141,34 @@ public class JSONProvider {
 			this.end = new StationView(tr.getStops().get(tr.getStops().size() - 1).getStation());
 		}
 
+		/**
+		 * Getter für id
+		 * @return id
+		 */
 		public String getId() {
 			return id;
 		}
 
+		/**
+		 * Getter für name
+		 * @return name
+		 */
 		public String getName() {
 			return name;
 		}
 
+		/**
+		 * Getter für begin
+		 * @return begin
+		 */
 		public StationView getBegin() {
 			return begin;
 		}
 
+		/**
+		 * Getter für end
+		 * @return end
+		 */
 		public StationView getEnd() {
 			return end;
 		}
@@ -139,6 +196,12 @@ public class JSONProvider {
 
 	}
 
+	/**
+	 * Methode zum Erstellen einer Liste von optimierten Bahnhofsdaten aus nicht optimierten Bahnhofsdaten.
+	 * @param input (Liste der nicht opimierten Bahnhofsdaten)
+	 * @return Liste der opimierten Bahnhofsdaten
+	 * @throws IllegalArgumentException
+	 */
 	public List<StationView> getStations(List<Station> input) throws IllegalArgumentException {
 		if (input == null)
 			throw new IllegalArgumentException("The given parameter is null!"); // TODO:
@@ -154,6 +217,12 @@ public class JSONProvider {
 		return ret;
 	}
 
+	/**
+	 * Methode zum Erstellen einer Liste von optimierten Zuglaufdaten aus nicht optimierten Zuglaufdaten.
+	 * @param input (Liste der nicht opimierten Zuglaufdaten)
+	 * @return Liste der opimierten Zuglaufdaten
+	 * @throws IllegalArgumentException
+	 */
 	public List<TrainRouteView> getTrainRoutes(List<TrainRoute> input) throws IllegalArgumentException {
 		if (input == null)
 			throw new IllegalArgumentException("The given parameter is null!"); // TODO:
