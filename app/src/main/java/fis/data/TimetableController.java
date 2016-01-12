59e68c2afb2d030dc3270a2eb9c515d0594218e9
luux <eric.schoelzel@yahoo.de>
2016-01-12 13:55:52 +0100

    added cleanup again

--- app/src/main/java/fis/data/TimetableController.java
+++ app/src/main/java/fis/data/TimetableController.java
@@ -453,8 +453,8 @@ public class TimetableController {
 	switch (event.getType()) {
 	case cleanup:
 	    // Löschen der bisherigen Datenstruktur
-	    //setTime(null);
-	    //resetData();
+	    setTime(null);
+	    resetData();
 	    break;
 	case parseRailML:
 	    resetData();
